802057ec235448990b0e878202ad2b7c8b557fb2
SaeedSatari <saeedsatari93@gmail.com>
2020-05-10 00:12:48 +0200

    -updated product-list.component.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "../common/product";
-import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +13,8 @@ export class ProductService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products))
-  }
-}
-
-interface GetResponse {
-  _embedded:{
-    products: Product[];
+  getProductList(): Observable<Product[]> {
+    // @ts-ignore
+    return this.httpClient.get(this.baseUrl).pipe()
   }
 }
